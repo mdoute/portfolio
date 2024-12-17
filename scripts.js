cf7570e9d33faf6325f82aedbb5c8e1df6597e60
mdoute <mdoute@gmail.com>
2024-12-17 18:38:39 -0500

    Added Web3Forms

--- scripts.js
+++ scripts.js
@@ -1,23 +1,38 @@
+const form = document.getElementById('form');
+const result = document.getElementById('result');
 
-// Form handler
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
 
-const form = document.getElementById("contact-form");
-form.addEventListener("submit", async (e) => {
-e.preventDefault();
-const formData = new FormData(form);
-const data = Object.fromEntries(formData);
-try {
-    const response = await fetch("https://8dgj440851.execute-api.us-east-2.amazonaws.com/ContactFormHandler", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-    });
-    if (response.ok) {
-    alert("Message sent successfully!"); // change this with a funciton that updates the website content to show a confirmation message
-    } else {
-    alert("Failed to send message. Please try again.");
-    }
-} catch (err) {
-    alert("An error occurred. Please try again.");
-}
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
 });
